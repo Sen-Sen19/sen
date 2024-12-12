b605ef86e4801b14581ff53d704c9edfd4bc88a3
Sen-Sen19 <marcneilsen021900@gmail.com>
2024-12-12 17:54:29 +0800

    Done

--- fetch_data.php
+++ fetch_data.php
@@ -9,6 +9,8 @@ $dbname = "fgls_db"; // Replace with your database name
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 
+
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
